f420717e787d221df102e67375b9e99afd678dc8
lu446529842 <446529842@qq.com>
2013-09-02 17:50:38 +0800

    pxe

---
+++ pxe/simple_pxe/install_pxe_ftp.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#PXE+DHCP+TFTP+KickStart delpoy redhat system
+#
+
+#=================================================
+#function install all require  package
+#=================================================
+function install_require_package(){
+ yum install -y  dhcp tftp-server nfs-utils syslinux vsftpd
+}
+
+
+#=================================================
+#function	config dhcp.conf
+#=================================================
+function config_dhcp(){
+ sed -e "s#<SUBNET>#${subnet}#g" -e "s#<RANGE_START>#${range_start}#g" -e "s#<RANGE_END>#${range_end}#g" -e "s#<FTP_SETVER>#${ftp_server}#g" ${BASE_DIR}/dhcpd.conf > /etc/dhcp/dhcpd.conf
+ service dhcpd start
+}
+
+
+
+#=================================================
+#function config ftp 
+#=================================================
+function config_ftp(){
+ cat ${BASE_DIR}/tftp > /etc/xinetd.d/tftp
+ service xinetd restart
+}
+
+
+#=================================================
+#function prepare for image
+#=================================================
+function prepare_image(){
+ mkdir /var/ftp/pub/rhel6
+ mkdir /tmp/ISO
+ mount -t iso9660 -o loop ${ISO_DIR} /tmp/ISO
+ echo "COPY ISO IMAGE TO /VAR/FTP/PUB DIR .PLS WAIT ...."
+ cp -R /tmp/ISO /var/ftp/pub/rhel6
+ cd /var/ftp/pub/rhel6 
+ mv ISO/* .
+ rm -rf ISO
+
+ cd /var/lib/tftpboot/
+ cp -a /var/ftp/pub/rhel6/isolinux/* .
+ mkdir pxelinux.cfg
+ sed "s#<FTP_SERVER>#${ftp_server}#g"  ${BASE_DIR}/default > /var/lib/tftpboot/pxelinux.cfg/default
+}
+
+
+#=================================================
+#function	config kickstart
+#=================================================
+function config_kickstart(){
+ sed "s#<FTP_SERVER>#${ftp_server}#g" ${BASE_DIR}/ks.cfg >  /var/ftp/pub/ks.cfg
+ chmod 644 /var/ftp/pub/ks.cfg
+ cp /usr/share/syslinux/pxelinux.0  /var/lib/tftpboot/
+ 
+ service vsftpd restart
+ service dhcpd restart
+ service xinetd restar
+}
+
+
+
+#=================================================
+#function main
+#=================================================
+function main(){
+ export BASE_DIR=$( pwd )
+ export ISO_DIR=""
+ export subnet=9.123.124.0
+ export range_start=9.123.124.2
+ export range_end=9.123.124.250
+ export ftp_server=$( ifconfig eth0  |grep "inet addr" |grep -v "127.0.0.1"|cut -d: -f2|awk '{print $1}' )
+ 
+ install_require_package
+ config_dhcp
+ config_ftp
+ prepare_image
+ config_kickstart
+}
---
+++ pxe/自动化安装原理/548247_files/cnzz_core.php
@@ -0,0 +1,15 @@
+(function(){
+try{
+var d=window._CNZZDbridge_4274540.bobject;
+var scheme =  (document.location.protocol=='https:')?'https:':'http:';
+d.callRequest([]);
+d.callScript([]);
+        d.createIcon([            "<a href='http://www.cnzz.com/stat/website.php?web_id=4274540' target=_blank title='&#31449;&#38271;&#32479;&#35745;'><img border=0 hspace=0 vspace=0 src='http://icon.cnzz.com/pic.gif'></a>"
+            ])}catch(err){
+}
+})();
+(function(){try{var n=['http://blog.51cto.com/'],e=document,f=window,m=encodeURIComponent,p="unknow",l=null,q=function(){this.init()};q.prototype={init:function(){if(!1===this.isValidUrl())return!1;var a;this.addHandler(e,"mousedown",this.clickHeat);a=f.navigator.userAgent;l=e.documentElement&&0!==e.documentElement.clientHeight?e.documentElement:e.body;a=a?a.toLowerCase().replace(/-/g,""):"";for(var b="netscape;se 1.;se 2.;saayaa;360se;tencent;qqbrowser;mqqbrowser;maxthon;myie;theworld;konqueror;firefox;chrome;safari;msie 5.0;msie 5.5;msie 6.0;msie 7.0;msie 8.0;msie 9.0;msie 10.0;Mozilla;opera".split(";"),
+d=0;d<b.length;d+=1)if(-1!==a.indexOf(b[d])){p=b[d];break}},addHandler:function(a,b,d){a.addEventListener?a.addEventListener(b,d,!1):a.attachEvent?a.attachEvent("on"+b,d):a["on"+b]=d},clickHeat:function(a){a||(a=f[a]);var b=a.target||a.srcElement;"IMG"===b.tagName&&(b=b.parentNode);var b="A"===b.tagName?1:0,d=a.which||a.button,k=a.clientX;a=a.clientY;var g=f.pageYOffset||l.scrollTop,k=k+(f.pageXOffset||l.scrollLeft);a+=g;var g=l.clientWidth||f.innerWidth,r=f.location.href,c=[];c.push("id=4274540");
+c.push("x="+k);c.push("y="+a);c.push("w="+g);c.push("s="+f.screen.width+"x"+f.screen.height);c.push("b="+p);c.push("c="+d);c.push("r="+m(e.referrer));c.push("a="+b);""!==f.location.hash&&c.push("p="+m(r));c.push("random="+m(Date()));var b=c.join("&"),h=new Image;h.onload=h.onerror=h.onabort=function(){h=h.onload=h.onerror=h.onabort=null};h.src="http://hm2.cnzz.com/heatmap.gif?"+b;return!0},isValidUrl:function(){var a=f.location.href,b=!1,d="([{\\^$|)?+.]}".split("");f.location.pathname||
+(a+="/");for(var k=0;k<n.length;k++){var g=n[k];if(-1!==g.indexOf("*")){for(var e=0;e<d.length;e++)var c="/\\"+d[e]+"/g",g=g.replace(eval(c),"\\"+d[e]);c="/\\*/g";g=g.replace(eval(c),"(.*)");c=RegExp(g,"i");if(c.test(a)){b=!0;break}}else if(g===a){b=!0;break}}return b}};new q}catch(s){}})();
+
